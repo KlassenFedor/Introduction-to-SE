c71048fb4a257675b704ed214cef0092d66c6b46
Klassen Fedor <fyodor.klassen@yandex.ru>
2022-06-04 14:07:08 +0700

    Добавлено начало обработки получения сообщений

--- ise_project/main.py
+++ ise_project/main.py
@@ -1,4 +1,5 @@
-from telethon.sync import TelegramClient
+from telethon.sync import TelegramClient, events
+from processing_commands import process_command
 import configparser
 
 config = configparser.ConfigParser()
@@ -9,3 +10,8 @@ api_hash = config['Telegram']['api_hash']
 username = config['Telegram']['username']
 
 client = TelegramClient(username, int(api_id), api_hash)
+
+
+@client.on(events.NewMessage())
+async def event_handler(event):
+    await process_command(event.message)
