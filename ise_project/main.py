59f1e4411df85f23142b1872d6989756952a711e
Klassen Fedor <fyodor.klassen@yandex.ru>
2022-06-07 11:52:27 +0700

    Добавлен master-аккаунт

--- ise_project/main.py
+++ ise_project/main.py
@@ -8,6 +8,7 @@ config.read("config.ini")
 api_id = config['Telegram']['api_id']
 api_hash = config['Telegram']['api_hash']
 username = config['Telegram']['username']
+main_account = config['Telegram']['main_account']
 
 client = TelegramClient(username, int(api_id), api_hash)
 
@@ -15,3 +16,8 @@ client = TelegramClient(username, int(api_id), api_hash)
 @client.on(events.NewMessage())
 async def event_handler(event):
     await process_command(event.message)
+
+
+client.start()
+client.send_message(main_account, 'Hello! Start of work.')
+client.run_until_disconnected()
