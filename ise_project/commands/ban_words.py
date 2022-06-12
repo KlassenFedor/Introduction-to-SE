5fdf449a07bf8f6ef51bdced27c169398fa30c13
Klassen Fedor <fyodor.klassen@yandex.ru>
2022-06-12 21:21:13 +0700

    Добавлены проверки на существование канала
    * Реализована проверка на существование запрашиваемого канала
    * Добавлены проверки в команды /ban_words, /messages_types

--- ise_project/commands/ban_words.py
+++ ise_project/commands/ban_words.py
@@ -1,10 +1,15 @@
+from commands.commands_helper import check_is_channel_exists
 from database import BanWord, ChannelBanWord, Channel
 
 
 async def main_command(client, tokens):
+    message = ''
+    if check_is_channel_exists(tokens[0]):
+        message = 'Такого канала не существует'
+        await client.send_message(message)
+        return
     channel_id = Channel.get(Channel.name == tokens[0]).id
     ban_words_ids = list(ChannelBanWord.get(ChannelBanWord.channel == channel_id).BanWord)
-    message = ''
     for ban_word in ban_words_ids:
         message += BanWord.get(BanWord.id == ban_word).word + ' '
 
--- ise_project/commands/messages_types.py
+++ ise_project/commands/messages_types.py
@@ -1,7 +1,13 @@
 import database
+from commands.commands_helper import check_is_channel_exists
 
 
 async def main_command(client, tokens):
+    message = ''
+    if check_is_channel_exists(tokens[0]):
+        message = 'Такого канала не существует'
+        await client.send_message(message)
+        return
     channel_id = database.Channel.get(database.Channel.name == tokens[0]).id
     message_type_ids = list(database.ChannelMessageType.get(database.ChannelMessageType.channel == channel_id).MessageType)
     message = ''
