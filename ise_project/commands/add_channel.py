5fed0ca123f53f2823e33af0920f6ae3f75bc763
Klassen Fedor <fyodor.klassen@yandex.ru>
2022-06-13 21:59:47 +0700

    Добавлены команды присоединения и выхода из канала
    * Добавлены функции для вступления и выхода из каналов
    * Реализованы проверки на то, является ли канал приватным

--- ise_project/commands/add_channel.py
+++ ise_project/commands/add_channel.py
@@ -1,2 +1,21 @@
+from telethon.tl.functions.messages import ImportChatInviteRequest
+from telethon.tl.functions.channels import JoinChannelRequest
+from telethon.errors import *
+
+
 async def main_command(client, tokens):
-    pass
+    try:
+        channel = tokens[0]
+        if len(tokens) == 2:
+            if tokens[1] == '-p':
+                try:
+                    await client(ImportChatInviteRequest(tokens[1].split('/')[-1]))
+                except Exception as e:
+                    message = 'Что-то пошло не так'
+                    await client.send_message(message)
+        await client(JoinChannelRequest(channel))
+    except Exception as e:
+        message = 'Что-то пошло не так'
+        if e == ChannelPrivateError:
+            message = 'Этот канал приватный, вам следует прислать ссылку-приглашение, чтобы присоединиться'
+        await client.send_message(message)
--- ise_project/commands/delete_channel.py
+++ ise_project/commands/delete_channel.py
@@ -1,2 +1,22 @@
+from telethon.errors import ChannelPrivateError
+from telethon.tl.functions.channels import LeaveChannelRequest
+
+from database import Channel
+
+
 async def main_command(client, tokens):
-    pass
+    channels = Channel.get()
+    channel_exists = False
+    for channel in channels:
+        if channel.channel_id == tokens[0]:
+            channel_exists = True
+            break
+    if channel_exists:
+        try:
+            await client(LeaveChannelRequest(tokens[0]))
+        except Exception as e:
+            message = 'Что-то пошло не так'
+            if e == ChannelPrivateError:
+                await client.delete_dialog(tokens[0])
+            else:
+                await client.send_message(message)
