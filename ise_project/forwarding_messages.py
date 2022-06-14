af5bd1f95a9ec611e4f635494a686d0f89cb0f75
Klassen Fedor <fyodor.klassen@yandex.ru>
2022-06-14 16:26:06 +0700

    Добавлена обработка сообщений при пересылке
    * Добавлены извлечения данных из сообщения для:
    * Проверки на бан-слова
    * Проверки на слова из белого списка
    * Проверки на соответствие типов сообщения

--- ise_project/forwarding_messages.py
+++ ise_project/forwarding_messages.py
@@ -1,6 +1,6 @@
 from telethon.sync import events
 
-from database import Channel
+from database import Channel, ChannelBanWord, BanWord, ChannelWhiteWord, WhiteWord, ChannelMessageType, MessageType
 from main import client, module, main_account
 
 
@@ -24,5 +24,48 @@ async def check_if_the_sender_being_tracked(sender):
     return False
 
 
-async def filtrate_message(message):
+async def get_message_types(message):
     pass
+
+
+async def has_ban_words(message, ban_words):
+    pass
+
+
+async def has_white_words(message, white_words):
+    pass
+
+
+async def has_message_types(types_in_message, channel_types):
+    pass
+
+
+async def filtrate_message(message):
+    sender = await client.get_entity(message.from_id.user_id)
+    message_text = message.text
+    message_types_in_message = get_message_types(message)
+
+    ban_words_ids = ChannelBanWord.get(ChannelBanWord.channel == sender)
+    ban_words = []
+    for ban_word_id in ban_words_ids:
+        ban_words = BanWord.get(BanWord.get_id == ban_word_id).select(BanWord.word)
+
+    white_words_ids = ChannelWhiteWord.get(ChannelWhiteWord.channel == sender)
+    white_words = []
+    for white_word_id in white_words_ids:
+        white_words = WhiteWord.get(WhiteWord.get_id == white_word_id).select(WhiteWord.word)
+
+    message_types_ids = ChannelMessageType.get(ChannelMessageType.channel == sender)
+    message_types = []
+    for message_type_id in message_types_ids:
+        message_types = MessageType.get(WhiteWord.get_id == message_type_id).select(MessageType.type)
+
+    if has_white_words(message_text, white_words):
+        return True
+    if has_ban_words(message_text, ban_words):
+        return False
+    if not has_message_types(message_types_in_message, message_types):
+        return False
+
+    return True
+
