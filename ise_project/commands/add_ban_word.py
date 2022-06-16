c69c9749a80e18bd55b35b5cddffb49d590402ff
Klassen Fedor <fyodor.klassen@yandex.ru>
2022-06-16 23:58:36 +0700

    Добавлена проверка существования бан-слова
    * Добавлены:
    * проверка на существование бан-слова
    * проверка на существования данного слова в списке указанного канала

--- ise_project/commands/add_ban_word.py
+++ ise_project/commands/add_ban_word.py
@@ -1,2 +1,20 @@
+from commands.commands_helper import *
+
+
 async def main_command(client, tokens):
-    pass
+    channel = tokens[0]
+    ban_word = tokens[1]
+    hashtag = tokens[2]
+    message = ''
+    if not check_is_channel_exists(tokens[0]):
+        message = 'Такого канала нет в отслеживаемых'
+        await client.sent_message(message)
+        return
+    if check_is_ban_word_exists(ban_word, channel):
+        message = 'Это слово уже существует'
+        await client.sent_message(message)
+        return
+
+    message = 'Слово добавлено, больше вы его не увидите в сообщениях от данного канала'
+    await client.sent_message(message)
+
--- ise_project/commands/commands_helper.py
+++ ise_project/commands/commands_helper.py
@@ -1,8 +1,16 @@
-from database import Channel
+from database import Channel, BanWord, ChannelBanWord
 
 
 def check_is_channel_exists(channel):
     current_channel = Channel.get(Channel.channel_id == channel)
     if len(current_channel) == 0:
+        return False
+    return True
+
+
+def check_is_ban_word_exists(ban_word, channel):
+    existing_ban_word = BanWord.get(BanWord.word == ban_word)
+    matched_ban_word_and_channel = ChannelBanWord.get(ChannelBanWord.get_id == existing_ban_word.id)
+    if matched_ban_word_and_channel is None:
         return False
     return True
